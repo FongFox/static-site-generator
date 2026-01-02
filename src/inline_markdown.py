dbb8a699445e2d0ee08c6540780c4b9600876878
FongFox <phong.tgn.coder@gmail.com>
2026-01-02 06:41:59 +0000

    feat(markdown): split text nodes into image and link nodes
    - add split_nodes_image to split TextType.TEXT nodes on markdown images
    - add split_nodes_link to split TextType.TEXT nodes on markdown links
    - reuse extract_markdown_images and extract_markdown_links for parsing
    - ensure non-TEXT nodes are preserved without modification
    - avoid creating empty TextNodes when splitting surrounding text
    - add tests covering multiple images/links and mixed content

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -30,14 +30,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if old_node.text_type != TextType.TEXT:
             node_list.append(TextNode(text=old_node.text, text_type=old_node.text_type))
             continue
+
         split_parts = old_node.text.split(delimiter)
         if len(split_parts) % 2 == 0:
             raise Exception("Invalid Markdown syntax")
+
         for index, part in enumerate(split_parts):
             if index % 2 == 0:
                 node_list.append(TextNode(text=part, text_type=TextType.TEXT))
                 continue
             node_list.append(TextNode(text=part, text_type=text_type))
+
     return node_list
 
 
@@ -46,12 +49,73 @@ def extract_markdown_images(text):
     return re.findall(r"!\[([^]]+)\]\(([^)]+)\)", text)
 
 
+def split_nodes_image(old_nodes):
+    node_list = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            node_list.append(old_node)
+            continue
+
+        image_nodes = extract_markdown_images(old_node.text)
+        if not image_nodes:
+            node_list.append(old_node)
+            continue
+
+        current_text = old_node.text
+        for img_alt, img_link in image_nodes:
+            pattern = f"![{img_alt}]({img_link})"
+            before, after = current_text.split(pattern, 1)
+            if before != "":
+                node_list.append(TextNode(text=before, text_type=TextType.TEXT))
+            node_list.append(
+                TextNode(text=img_alt, text_type=TextType.IMAGE, url=img_link)
+            )
+            current_text = after
+
+        if current_text != "":
+            node_list.append(TextNode(text=current_text, text_type=TextType.TEXT))
+
+    return node_list
+
+
 def extract_markdown_links(text):
     # TODO: tương tự nhưng cho link thường
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 
+def split_nodes_link(old_nodes):
+    node_list = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            node_list.append(old_node)
+            continue
+
+        link_nodes = extract_markdown_links(old_node.text)
+        if not link_nodes:
+            node_list.append(old_node)
+            continue
+
+        current_text = old_node.text
+        for link_text, link_url in link_nodes:
+            pattern = f"[{link_text}]({link_url})"
+            before, after = current_text.split(pattern, 1)
+            if before != "":
+                node_list.append(TextNode(text=before, text_type=TextType.TEXT))
+            node_list.append(
+                TextNode(text=link_text, text_type=TextType.LINK, url=link_url)
+            )
+            current_text = after
+
+        if current_text != "":
+            node_list.append(TextNode(text=current_text, text_type=TextType.TEXT))
+
+    return node_list
+
+
 # if __name__ == "__main__":
-#     sample_text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and [to boot dev](https://www.boot.dev)"
-#     print("Images:", extract_markdown_images(sample_text))
-#     print("Links:", extract_markdown_links(sample_text))
+#     node = TextNode(
+#         "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+#         TextType.TEXT,
+#     )
+#     new_nodes = split_nodes_image([node])

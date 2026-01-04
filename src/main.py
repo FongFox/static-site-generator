82eb6a81e62009d709c6c65d24da2ce2202386ad
FongFox <phong.tgn.coder@gmail.com>
2026-01-04 12:17:22 +0000

    feat(static-site-gen): Implement recursive directory copy for static assets
    This commit introduces a new feature to the static site generator: the ability to recursively copy
    all static assets from a source directory (e.g., 'static/') to a destination directory (e.g., 'public/').
    
    Key changes include:
    - A `copy_directory_contents` function that handles recursive copying.
    - Deletion of the destination directory prior to copying to ensure a clean build.
    - Creation of destination subdirectories as needed during the recursive process.
    - Robust error handling for non-existent source directories.
    - Utilizes `os` and `shutil` Python modules for file system operations.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def copy_directory_contents(source_dir, dest_dir):
+    cwd = os.getcwd()
+    src_dir_path = os.path.join(cwd, source_dir)
+    dest_dir_path = os.path.join(cwd, dest_dir)
+
+    if not os.path.exists(src_dir_path):
+        raise Exception("Folder path not existed!")
+
+    if os.path.exists(dest_dir_path):
+        shutil.rmtree(dest_dir_path)
+
+    os.mkdir(dest_dir_path)
+
+    src_dir = os.listdir(src_dir_path)
+    for item in src_dir:
+        cur_path = os.path.join(src_dir_path, item)
+        if os.path.isfile(cur_path):
+            shutil.copy(cur_path, dest_dir_path)
+        else:
+            new_des_dir_path = os.path.join(dest_dir_path, item)
+            os.mkdir(new_des_dir_path)
+            copy_directory_contents(cur_path, new_des_dir_path)
 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    copy_directory_contents("static", "public")
+    pass
 
 
 if __name__ == "__main__":
